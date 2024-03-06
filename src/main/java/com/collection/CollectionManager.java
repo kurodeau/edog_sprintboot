cc12afdbe406823cf625673eefafda8ce3cd1256
Kuro.Wu <kurome123@gmail.com>
2024-03-06 20:37:38 +0800

    chore set/test redis

---
+++ src/main/java/com/collection/CollectionManager.java
@@ -0,0 +1,17 @@
+package com.collection;
+
+import redis.clients.jedis.Jedis;
+import java.util.TreeSet;
+import com.google.gson.Gson;
+
+public class CollectionManager {
+
+	public CollectionManager() {
+		super();
+	}
+	
+	public void main() {};
+
+	
+	
+}
