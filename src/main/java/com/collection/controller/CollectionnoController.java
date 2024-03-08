3f0569b87ac03c302e257ac48a595739785b4f89
Kuro.Wu <kurome123@gmail.com>
2024-03-08 10:08:46 +0800

    chore finsih Jedis -> Thymleaf show

--- src/main/java/com/collection/controller/CollectionnoController.java
+++ src/main/java/com/collection/controller/CollectionnoController.java
@@ -63,6 +63,11 @@ public class CollectionnoController {
 
 		// 測試用的, 建立連線池
 		JedisPool jedisPool = JedisUtil.getJedisPool();
+		
+		// 本方法會用到的變數
+		List<ProductVO> productList = new ArrayList();
+		ProductVO product = new ProductVO();
+		Map<String, List<ProductVO>> collectionClassfi = new HashMap<>();
 
 		// 索取redis連線, 用try 整個包起來
 		try (
@@ -75,18 +80,20 @@ public class CollectionnoController {
 
 			// 將所有收藏的資料包入 List<ProductVO>, 並透過公司名稱分為Map
 //			Set<String> sellerCompanySet = new HashSet();
-			ProductVO product = new ProductVO();
-			Map<String,List<ProductVO>> collectionClassfi = new HashMap<>();
+
 			for (String str : jedis.lrange(memberId, 0, -1)) {
 				product = productSvc.getOneProduct(Integer.parseInt(str));
 				String sellerCompany = product.getSellerVO().getSellerCompany();
-//				sellerCompanySet.add(sellerCompany);
-//				collectionClassfi.
-//				product.set
-//				collectList.add(product);
-			}
 
-	        // 將 collectList 中的 Product 根据 sellerId 分组, 不知道怎麼用Stream寫
+				// 將每個 ProductVO 透過 "sellerCompany" 鍵關聯起来
+				productList = collectionClassfi.getOrDefault(
+						sellerCompany, new ArrayList<>()); // 拉出對應Key的value List，再對其更新
+				productList.add(product);
+				collectionClassfi.put(sellerCompany, productList);
+			}
+			System.out.println("測試" + collectionClassfi.toString()); //有成功分類並包成Map
+
+			// 將 collectList 中的 Product 根据 sellerId 分组, 不知道怎麼用Stream寫
 //	        Map<String, List<ProductVO> > groupedProducts = collectList.stream()
 //	                .collect(Collectors.groupingBy(ProductVO::getSellerCompany ));
 
@@ -99,7 +106,7 @@ public class CollectionnoController {
 //            // 無論連線操作是否成功都釋放當前的 Jedis 連線，將其返回到連線池中
 //			jedisPool.close();
 //		}
-		model.addAttribute("collectList", collectList);
+		model.addAttribute("collectionClassfi", collectionClassfi);
 		return "front-end/buyer/buyer-collection-list";
 	}
 
