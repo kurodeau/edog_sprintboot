f59452a749cba088ce58a0242c34ea5111d33e8b
Nick <zxcv33398377@gmail.com>
2024-03-13 15:06:29 +0800

    假的

--- src/main/java/com/petdraw/model/PetDrawRepository.java
+++ src/main/java/com/petdraw/model/PetDrawRepository.java
@@ -1,13 +1,9 @@
 package com.petdraw.model;
 
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.buyer.entity.BuyerVO;
 
 public interface PetDrawRepository extends JpaRepository<PetDrawVO, Integer> {
 	@Transactional
