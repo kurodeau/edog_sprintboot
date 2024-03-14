5ce79050fb15d73819fa38f36206b8f2ce6d8829
Kuro.Wu <kurome123@gmail.com>
2024-03-14 21:10:14 +0800

    chore finish creat order Redis

--- src/main/java/com/cart/controller/CartnoController.java
+++ src/main/java/com/cart/controller/CartnoController.java
@@ -1,35 +1,35 @@
-	package com.cart.controller;
+package com.cart.controller;
 
 import java.util.*;
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.validation.Valid;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.origin.SystemEnvironmentOrigin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.cart.service.CartService;
-import com.collection.service.CollectionService;
 import com.product.model.ProductService;
-import com.product.model.ProductVO;
-import com.redis.JedisUtil;
-import com.seller.entity.SellerVO;
 import com.seller.service.SellerService;
-
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+import com.util.HttpResult;
 
 @Controller
 @RequestMapping("/front/buyer/cart")
@@ -45,7 +45,7 @@ public class CartnoController extends HttpServlet {
 	CartService cartService;
 
 	
-	// 用戶取出自己所有購物車資料 /front/buyer/cart/list
+	//	用戶取出自己所有購物車資料 /front/buyer/cart/list
 	@GetMapping("list")
 	public String cartlist(String memberId, Model model) {
 		// 先給定 memberId 以便測試
@@ -53,6 +53,48 @@ public class CartnoController extends HttpServlet {
 		model.addAttribute("cartClassfi", cartService.getAllByMemberId(memberId)); 
 		return "front-end/buyer/buyer-cart-list";
 	}
+	
+	
+	
+	//	收到購物車的結帳請求, 把資料整理後上Redis /front/buyer/cart/cart_creat_order
+	@RequestMapping(value = "cart_creat_order", method = RequestMethod.POST)	
+	public ResponseEntity<?> yourControllerMethod(@RequestBody String jsonData) {
+//        System.out.println("有到cart_creat_order contrller");
+		JSONObject jsonObject = new JSONObject(jsonData);
+        System.out.println(jsonData);
+        if(jsonData==null || jsonData.equals("{}")){
+            HttpResult<String> result = new HttpResult<>(400, "", "FUCKU");
+            return ResponseEntity.badRequest().body(result);
+        }
+        
+        //=================原本用來寫依照商品名稱分類的作法 start
+//        List<String> sellerNames =  new ArrayList();
+//          
+//        System.out.println("---");
+//        for (String seller : jsonObject.keySet()) {
+//        	sellerNames.add(seller);
+//            System.out.println("Seller: " + seller);
+//            JSONArray productList = jsonObject.getJSONArray(seller);
+//            for (int i = 0; i < productList.length(); i++) {
+////            	CartVO cartVO  = new ProductVO();
+//                JSONObject product = productList.getJSONObject(i);
+//                int productId = product.getInt("productId");
+//                int productQty = product.getInt("productQty");
+//                System.out.println("productId: " + productId + ", productQty: " + productQty);
+//            }
+//        }
+        //=================原本用來寫依照商品名稱分類的作法 end
+        
+        // 呼叫生成order Redis 資料的方法
+        cartService.creatOrderByMemberId( jsonObject );
+       
+        
+	    // 返回適當的響應
+	    return ResponseEntity.ok().body(new HttpResult<>(200,"","success"));
+	    
+	    
+	}
+	
 
 	// 更新特定一個商品編號的收藏狀態, 並回到我的收藏 /front/buyer/collection/switchState
 //	@PostMapping("switchState") //改用POST
--- src/main/java/com/cart/service/CartService.java
+++ src/main/java/com/cart/service/CartService.java
@@ -1,21 +1,12 @@
 package com.cart.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonArray;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -27,6 +18,7 @@ import com.product.model.ProductService;
 import com.product.model.ProductVO;
 import com.redis.JedisUtil;
 import com.seller.entity.SellerVO;
+import com.util.HttpResult;
 import com.cart.model.*;
 
 import redis.clients.jedis.Jedis;
@@ -39,7 +31,7 @@ public class CartService {
 
 	@Autowired
 	ProductService productSvc;
-	
+
 	// List + DTO作法, 獲取mamber所有購物車資訊
 //	public Map<String, Set<CartVO>> getAllByMemberId(String memberId) {
 //
@@ -106,10 +98,10 @@ public class CartService {
 	public Map<String, Set<CartVO>> getAllByMemberId(String memberId) {
 
 		memberId = "8";
-		
+
 		// 設定redisKey, 取得連線
-		String redisKey = "cart:"+ memberId;
-		System.out.println("redisKey= " + redisKey); //測試訊息
+		String redisKey = "cart:" + memberId;
+		System.out.println("redisKey= " + redisKey); // 測試訊息
 		JedisPool jedisPool = JedisUtil.getJedisPool();
 
 		// 本方法會用到的跨區域變數
@@ -119,8 +111,8 @@ public class CartService {
 
 		// 索取redis連線, 用try 整個包起來
 		try (
-			// 從 Redis 中讀取資料 並且指定為db10
-			Jedis jedis = jedisPool.getResource()) {
+				// 從 Redis 中讀取資料 並且指定為db10
+				Jedis jedis = jedisPool.getResource()) {
 			jedis.select(10);
 
 			// 獲得該筆Redis 內容Keys
@@ -131,33 +123,32 @@ public class CartService {
 			String sellerCompany;
 			SellerVO sellerVO;
 			String productNum;
-						
+
 //			//===== 先計算List中每個商品編號有幾個
 //			Map<String, Integer> productAndNum = new HashMap<>();
 //			for (String p : redisData) {
 //				productAndNum.put(p, productAndNum.getOrDefault(p, 0) + 1);
 //			}
-            //===== 把Keys Map 中的值 實體化為要回傳的CartVO
-		    for (String productId : allProductId) 
-		    {
-	            System.out.println("service測試訊息,productId: " + productId );
-	            System.out.println("service測試訊息,productId 數量value: " + jedis.hget(redisKey, productId) );
-	            CartVO cartVO = new CartVO();    
-	            ProductVO productVO = productSvc.getOneProduct( Integer.valueOf( productId ) );
-	            cartVO.setProductVO(productVO);
-	            sellerCompany = productVO.getSellerVO().getSellerCompany();
-	            cartVO.setSellerCompany( sellerCompany );
-	            cartVO.setSellerVO( productVO.getSellerVO() );
-	            cartVO.setProductNum( jedis.hget(redisKey, productId).toString() );
+			// ===== 把Keys Map 中的值 實體化為要回傳的CartVO
+			for (String productId : allProductId) {
+				System.out.println("service測試訊息,productId: " + productId);
+				System.out.println("service測試訊息,productId 數量value: " + jedis.hget(redisKey, productId));
+				CartVO cartVO = new CartVO();
+				ProductVO productVO = productSvc.getOneProduct(Integer.valueOf(productId));
+				cartVO.setProductVO(productVO);
+				sellerCompany = productVO.getSellerVO().getSellerCompany();
+				cartVO.setSellerCompany(sellerCompany);
+				cartVO.setSellerVO(productVO.getSellerVO());
+				cartVO.setProductNum(jedis.hget(redisKey, productId).toString());
 //	            System.out.println( "測試cartVO=" + cartVO.toString() );
 
 				// 將每個 CartVO 透過 "sellerCompany" 鍵關聯起来
 //	            System.out.println("將每個 CartVO 透過 \"sellerCompany\" 鍵關聯起来");
-	            cartSet = cartClassfi.getOrDefault(cartVO.getSellerCompany(), new HashSet<CartVO>());
-	            cartSet.add(cartVO);
+				cartSet = cartClassfi.getOrDefault(cartVO.getSellerCompany(), new HashSet<CartVO>());
+				cartSet.add(cartVO);
 				cartClassfi.put(sellerCompany, cartSet);
-	        }
-			
+			}
+
 		} catch (Exception e) {
 			System.out.println("service從redis讀出資料有問題");
 			e.printStackTrace();
@@ -165,8 +156,64 @@ public class CartService {
 //		System.out.println("回傳 cartClassfi"); // 測試資料
 		return cartClassfi;
 	}
-	
-	
+
+	// Redis 存的格式等同Cart:[memberId]
+	public void creatOrderByMemberId(JSONObject jsonData) {
+//        System.out.println("有進入creatOrderByMemberId方法");
+//		JSONObject jsonObject = new JSONObject(jsonData);
+		if (jsonData == null || jsonData.equals("{}")) {
+			HttpResult<String> result = new HttpResult<>(400, "", "FUCKU");
+			System.out.println("controller 來的 JSON 的資料是空的???");
+		}
+
+		// =================整理 JSON 資料, 拿出memberId start
+		List<String> members = new ArrayList();
+		Map<String, String> orderData = new HashMap<>();
+		String memberId = "1";
+		System.out.println("---");
+		for (String member : jsonData.keySet()) {
+			members.add(member);
+			memberId = member;
+			System.out.println("member: " + member);
+			JSONArray memberList = jsonData.getJSONArray(member);
+			for (int i = 0; i < memberList.length(); i++) {
+//        	CartVO cartVO  = new ProductVO();
+				JSONObject product = memberList.getJSONObject(i);
+				String productId = String.valueOf(product.getInt("productId"));
+				String productQty = String.valueOf(product.getInt("productQty"));
+				orderData.put(productId, productQty);
+				System.out.println("productId: " + productId + ", productQty: " + productQty);
+			}
+		}
+		// =================整理 JSON 資料 end
+		// =================創建Redis 資料 start
+		// 設定redisKey, 取得連線
+		String redisKey = "order:" + memberId;
+		System.out.println("redisKey= " + redisKey); // 測試訊息
+		JedisPool jedisPool = JedisUtil.getJedisPool();
+
+		// 索取redis連線, 用try 整個包起來
+		try (
+			// 從 Redis 中讀取資料 並且指定為db10
+			Jedis jedis = jedisPool.getResource()) {
+			jedis.select(10);
+
+			// 檢查該會員是否已經有訂單, 如果有就移除, 然後塞入新的值
+			if (jedis.exists(redisKey)) {
+				jedis.del(redisKey);
+			}
+			System.out.println("打資料上Redis前最後確認:" + redisKey + orderData);
+			jedis.hmset(redisKey, orderData);
+			// =================創建Redis 資料 end
+
+		} catch (Exception e) {
+			System.out.println("service對Redis的操作有問題");
+			e.printStackTrace();
+		}
+//		System.out.println("回傳 cartClassfi"); // 測試資料
+		System.out.println("這個沒有return 到這裡order redis資料 已經成立");
+	}
+
 	// 這個還沒完成
 	public void memberAddOneByProductId(String memberId, String prouductId) {
 
@@ -212,14 +259,14 @@ public class CartService {
 		System.out.println("方法結束, 沒有return");
 	}
 
-	// List + DTO作法 
+	// List + DTO作法
 	public Map<String, Set<CartVO>> removeOneFromCart(String memberId, String prouductId) {
 
 		memberId = "9";
-		
+
 		// 設定redisKey, 取得連線
-		String redisKey = "cart"+ memberId;
-		System.out.println("redisKey= " + redisKey); //測試訊息
+		String redisKey = "cart" + memberId;
+		System.out.println("redisKey= " + redisKey); // 測試訊息
 		JedisPool jedisPool = JedisUtil.getJedisPool();
 
 		// 本方法會用到的跨區域變數
@@ -229,8 +276,8 @@ public class CartService {
 
 		// 索取redis連線, 用try 整個包起來
 		try (
-			// 從 Redis 中讀取資料 並且指定為db10
-			Jedis jedis = jedisPool.getResource()) {
+				// 從 Redis 中讀取資料 並且指定為db10
+				Jedis jedis = jedisPool.getResource()) {
 			jedis.select(10);
 
 			redisData = jedis.lrange(redisKey, 0, -1);
@@ -238,16 +285,16 @@ public class CartService {
 
 			// 宣告要裝入回傳List<CartVO> 的參數
 			String sellerCompany;
-				
+
 			SellerVO sellerVO;
 			String productNum;
-			
-			//===== 先計算List中每個商品編號有幾個
+
+			// ===== 先計算List中每個商品編號有幾個
 			Map<String, Integer> productAndNum = new HashMap<>();
 			for (String p : redisData) {
 				productAndNum.put(p, productAndNum.getOrDefault(p, 0) + 1);
 			}
-			
+
 //			//===== 移除指定的Key 與對應的內容
 //			String keyToRemove = prouductId;			
 //			if (productAndNum.containsKey(keyToRemove)) {
@@ -258,26 +305,26 @@ public class CartService {
 //			    // 如果不存在，印出訊息
 //			    System.out.println("Key: " + keyToRemove + " 不存在於 Map 中");
 //			}
-			
-            //===== 把Mep中的資 實體化為要回傳的CartVO
-		    for (Map.Entry<String, Integer> p : productAndNum.entrySet()) {
-	            System.out.println("測試訊息,Element: " + p.getKey() + ", Count: " + p.getValue());
-	            CartVO cartVO = new CartVO();    
-	            ProductVO productVO = productSvc.getOneProduct( Integer.valueOf( p.getKey() ) );
-	            cartVO.setProductVO(productVO);
-	            sellerCompany = productVO.getSellerVO().getSellerCompany();
-	            cartVO.setSellerCompany( sellerCompany );
-	            cartVO.setSellerVO( productVO.getSellerVO() );
-	            cartVO.setProductNum( p.getValue().toString() );
+
+			// ===== 把Mep中的資 實體化為要回傳的CartVO
+			for (Map.Entry<String, Integer> p : productAndNum.entrySet()) {
+				System.out.println("測試訊息,Element: " + p.getKey() + ", Count: " + p.getValue());
+				CartVO cartVO = new CartVO();
+				ProductVO productVO = productSvc.getOneProduct(Integer.valueOf(p.getKey()));
+				cartVO.setProductVO(productVO);
+				sellerCompany = productVO.getSellerVO().getSellerCompany();
+				cartVO.setSellerCompany(sellerCompany);
+				cartVO.setSellerVO(productVO.getSellerVO());
+				cartVO.setProductNum(p.getValue().toString());
 //	            System.out.println( "測試cartVO=" + cartVO.toString() );
 
 				// 將每個 CartVO 透過 "sellerCompany" 鍵關聯起来
 //	            System.out.println("將每個 CartVO 透過 \"sellerCompany\" 鍵關聯起来");
-	            cartSet = cartClassfi.getOrDefault(cartVO.getSellerCompany(), new HashSet<CartVO>());
-	            cartSet.add(cartVO);
+				cartSet = cartClassfi.getOrDefault(cartVO.getSellerCompany(), new HashSet<CartVO>());
+				cartSet.add(cartVO);
 				cartClassfi.put(sellerCompany, cartSet);
-	        }
-			
+			}
+
 		} catch (Exception e) {
 			System.out.println("從redis讀出資料有問題");
 			e.printStackTrace();
@@ -285,6 +332,5 @@ public class CartService {
 //		System.out.println("回傳 cartClassfi"); // 測試資料
 		return cartClassfi;
 	}
-	
-	
+
 }
