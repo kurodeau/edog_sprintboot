bb4df5d98e44b0d6ab61ca7cfe3d1530abb0adfb
Nick <zxcv33398377@gmail.com>
2024-03-20 03:57:12 +0800

    修正論壇部分的config petdraw修改html的style

--- src/main/java/com/interceptor/InterceptorConfig.java
+++ src/main/java/com/interceptor/InterceptorConfig.java
@@ -48,8 +48,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/buyer/order_checkout111");
 		    
     
-		// 論壇部分
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/listAll"); 
+
 
     
 		// 後台部分
@@ -59,17 +58,13 @@ public class InterceptorConfig implements WebMvcConfigurer {
 		
 		// 論壇部分
 		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/listAll");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/TypeList?id=1");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/TypeList?id=2");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/TypeList?id=3");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/TypeList?id=4");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/TypeList?id=5");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/TypeList?id=6");
+		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/article/{id}");
 		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/article/add");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/article/MyArticle?id=1");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/articleLike/MyArticleLike?id=1");
-		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/msg/MyMsg?id=1");
+		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/article/{id}");
+		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/articleLike/{id}");
+		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/msg/{id}");
 		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/petdraw");
+		registry.addInterceptor(changeHeaderInfoInterceptor).addPathPatterns("/front/forum/petdraw/{id}");
 
 	
 
